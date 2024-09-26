b9f9be594d6473cb9ac1a30f99d55013f4df28ab
harryszh <harrys.zh12@gmail.com>
2024-09-26 10:14:24 +0200

    kochrezepte

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    <header>
+        <input placeholder="Suche..">
+     </header>
+
+
+
+</template>
+
+<script>
+    export default{
+    name: 'Header'
+    }
+</script>
+
+<style scoped>
+    header{
+        background-color: goldenrod;
+        height: 150px;
+        padding: 16px;
+
+        display: flex;
+        align-items: flex-end;
+        border-bottom-left-radius: 8px;
+        border-bottom-right-radius: 8px;
+    }
+
+
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+    <RecipeItem/>
+    <RecipeItem/>
+    <RecipeItem/>
+    <RecipeItem/>
+</template>
+
+<script>
+
+
+import RecipeItem from'@/components/RecipeItem'
+export default {
+  components: {
+    RecipeItem
+  }
+}
+</script>
